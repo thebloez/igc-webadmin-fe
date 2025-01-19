67aee77e6a11e55ab1da78005e4368693bbed055
Lukman Hidayah <lukman.hidayah@ottodigital.id>
2025-01-19 14:13:51 +0700

    Update script references in index.html and enhance memo item styling for better readability

--- src/presentation/components/memo/PrintMemoItem.tsx
+++ src/presentation/components/memo/PrintMemoItem.tsx
@@ -12,13 +12,13 @@ export const PrintMemoItem: React.FC<PrintMemoItemProps> = ({
   return (
     <div className="tw-flex tw-justify-between tw-font-bold tw-py-[2px]">
       <div className="tw-w-[30%]">
-        <p className="tw-text-lg">{label}</p>
+        <p className="tw-text-xl">{label}</p>
       </div>
       <div className="tw-w-[5%]">
         <p>:</p>
       </div>
       <div className="tw-w-[65%]">
-        <p className="tw-text-lg">{isNullOrEmpty(value) ? "-" : value}</p>
+        <p className="tw-text-xl">{isNullOrEmpty(value) ? "-" : value}</p>
       </div>
     </div>
   );
